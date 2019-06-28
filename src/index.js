e6a531d27bfa25f400743458f4257a9f5f101d94
Afroze Khan <a.froze.ak@gmail.com>
2019-06-28 16:52:23 -0400

    added bootstrap and logger

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+
+// import logging
+import * as log from 'loglevel';
+
+log.enableAll();
+log.trace("logger trace test");
+log.debug("logger debug test");
+log.info("logger info test");
+log.warn("logger warn test");
+log.error("logger error test");
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
